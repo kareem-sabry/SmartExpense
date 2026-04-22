beae70eb0e5886a235da98adabe78a6f3ff61a41
kay5070 <karemsabry5@gmail.com>
2026-04-22 19:01:38 +0200

    refactor(api): extract identity configuration into extension method

---
+++ SmartExpense.Api/Extensions/IdentityExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SmartExpense.Core.Constants;
+using SmartExpense.Core.Entities;
+using SmartExpense.Infrastructure.Data;
+
+namespace SmartExpense.Api.Extensions;
+
+public static class IdentityExtensions
+{
+    public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+    {
+        services.AddIdentity<User, IdentityRole<Guid>>(opt =>
+            {
+                opt.Password.RequireDigit = true;
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireUppercase = true;
+                opt.Password.RequireNonAlphanumeric = true;
+                opt.Password.RequiredLength = ApplicationConstants.MinPasswordLength;
+
+                opt.User.RequireUniqueEmail = true;
+
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+                opt.Lockout.AllowedForNewUsers = true;
+
+                opt.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultProvider;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<DataProtectionTokenProviderOptions>(opt =>
+        {
+            opt.TokenLifespan =
+                TimeSpan.FromHours(ApplicationConstants.PasswordResetTokenExpirationHours);
+        });
+
+        return services;
+    }
+}
