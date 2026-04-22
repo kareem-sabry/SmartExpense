5e7713bf83e305190b60fd32ec344e6ff087a3c0
kay5070 <karemsabry5@gmail.com>
2026-04-22 19:05:11 +0200

    refactor(api): extract Swagger and API versioning configuration

---
+++ SmartExpense.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Asp.Versioning;
+using Microsoft.OpenApi.Models;
+
+namespace SmartExpense.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new UrlSegmentApiVersionReader(),
+                    new HeaderApiVersionReader("X-Api-Version"));
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SmartExpense API",
+                Version = "v1",
+                Description = "A Smart Expense Tracker and analyzer - Version 1",
+                Contact = new OpenApiContact
+                {
+                    Name = "SmartExpense Support",
+                    Email = "support@smartexpense.com"
+                }
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter 'Bearer' [space] and then your JWT token."
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+}
