0b6860869d495888eeccc6d681f64db7d34ba7af
kay5070 <karemsabry5@gmail.com>
2026-04-22 19:05:52 +0200

    refactor(api): consolidate application service registrations into extension method

---
+++ SmartExpense.Api/Extensions/AppServicesExtensions.cs
@@ -0,0 +1,40 @@
+﻿using SmartExpense.Application.Interfaces;
+using SmartExpense.Core.Models;
+using SmartExpense.Infrastructure.Data;
+using SmartExpense.Infrastructure.Services;
+
+namespace SmartExpense.Api.Extensions;
+
+
+public static class AppServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Options
+        services.Configure<JwtOptions>(
+            configuration.GetSection(JwtOptions.JwtOptionsKey));
+        services.Configure<AdminUserOptions>(
+            configuration.GetSection("AdminUser"));
+        services.Configure<EmailOptions>(
+            configuration.GetSection("EmailOptions"));
+
+        // Infrastructure
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddScoped<IAuthTokenProcessor, AuthTokenProcessorService>();
+
+        // Domain services
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddScoped<ITransactionService, TransactionService>();
+        services.AddScoped<IRecurringTransactionService, RecurringTransactionService>();
+        services.AddScoped<IAnalyticsService, AnalyticsService>();
+        services.AddScoped<IBudgetService, BudgetService>();
+        services.AddScoped<IAdminService, AdminService>();
+        services.AddScoped<IEmailService, EmailService>();
+
+        return services;
+    }
+}
