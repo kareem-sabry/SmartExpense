759c562e1ef30bb8e75c618440590101afb534ad
kay5070 <karemsabry5@gmail.com>
2026-04-25 22:16:04 +0300

    fix: relax CSP to allow Swagger UI inline scripts and styles

--- src/SmartExpense.Api/Extensions/MiddlewareExtensions.cs
+++ src/SmartExpense.Api/Extensions/MiddlewareExtensions.cs
@@ -6,22 +6,28 @@ namespace SmartExpense.Api.Extensions;
 
 public static class MiddlewareExtensions
 {
-    public static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder app)
+    public static void UseSecurityHeaders(this IApplicationBuilder app)
     {
-        return app.Use(async (context, next) =>
+        app.Use(async (context, next) =>
         {
             context.Response.Headers["X-Content-Type-Options"] = "nosniff";
             context.Response.Headers["X-Frame-Options"] = "DENY";
             context.Response.Headers["X-XSS-Protection"] = "1; mode=block";
             context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
-            context.Response.Headers["Content-Security-Policy"] = "default-src 'self'";
+
+            context.Response.Headers["Content-Security-Policy"] =
+                "default-src 'self'; " +
+                "script-src 'self' 'unsafe-inline'; " +
+                "style-src 'self' 'unsafe-inline'; " +
+                "img-src 'self' data:;";
+
             await next();
         });
     }
 
-    public static IApplicationBuilder UseStructuredRequestLogging(this IApplicationBuilder app)
+    public static void UseStructuredRequestLogging(this IApplicationBuilder app)
     {
-        return app.UseSerilogRequestLogging(options =>
+        app.UseSerilogRequestLogging(options =>
         {
             options.MessageTemplate =
                 "HTTP {RequestMethod} {RequestPath} → {StatusCode} in {Elapsed:0.0000} ms";
