1dae4368d58bad606d12b92c441619d3ef622160
kay5070 <karemsabry5@gmail.com>
2026-04-25 18:09:03 +0300

    fix : add lambda to get the current time each time the Rule runs

---
+++ SmartExpense.Application/Validators/Transaction/TransactionCreateDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using SmartExpense.Application.Dtos.Transaction;
+
+namespace SmartExpense.Application.Validators.Transaction;
+
+public class TransactionCreateDtoValidator : AbstractValidator<TransactionCreateDto>
+
+{
+    public TransactionCreateDtoValidator()
+    {
+        RuleFor(x => x.CategoryId)
+            .GreaterThan(0).WithMessage("A valid category must be selected.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MinimumLength(1).WithMessage("Description cannot be empty.")
+            .MaximumLength(200).WithMessage("Description cannot exceed 200 characters.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.")
+            .ScalePrecision(2, 18)
+            .WithMessage("Amount cannot have more than 2 decimal places.");
+
+        RuleFor(x => x.TransactionType)
+            .IsInEnum().WithMessage("Transaction type must be Income or Expense.");
+
+        RuleFor(x => x.TransactionDate)
+            .NotEmpty().WithMessage("Transaction date is required.")
+            .LessThanOrEqualTo(_=> DateTime.UtcNow)
+            .WithMessage("Transaction date cannot be in the future.");
+
+        RuleFor(x => x.Notes)
+            .MaximumLength(500).WithMessage("Notes cannot exceed 500 characters.")
+            .When(x => x.Notes is not null);
+    }
+}
